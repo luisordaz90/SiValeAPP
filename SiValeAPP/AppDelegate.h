ffb8184d7148876874fc8df64e6f22bf9b72c291
Luis Ordaz <luis.rdaz@gmail.com>
2015-07-05 13:23:00 -0500

    Initial Commit

---
+++ SiValeAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SiValeAPP
+//
+//  Created by Luis Ordaz on 7/5/15.
+//  Copyright (c) 2015 Luis Ordaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SiValeAPP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SiValeAPP
+//
+//  Created by Luis Ordaz on 7/5/15.
+//  Copyright (c) 2015 Luis Ordaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
